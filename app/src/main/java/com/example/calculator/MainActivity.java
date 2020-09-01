a86777db5ef3fe5fca1fd10dfed8f43ff00a409b
Kamal <kamal200107@hotmail.com>
2020-09-01 03:37:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isOpPressed = false;
+
+    private double firstNum = 0;
+
+    private int secondNumberIndex = 0;
+
+    private char currentOp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView calcScreen = findViewById(R.id.calculatorScreen);
+        final Button n0 = findViewById(R.id.n0);
+        final Button n1 = findViewById(R.id.n1);
+        final Button n2 = findViewById(R.id.n2);
+        final Button n3 = findViewById(R.id.n3);
+        final Button n4 = findViewById(R.id.n4);
+        final Button n5 = findViewById(R.id.n5);
+        final Button n6 = findViewById(R.id.n6);
+        final Button n7 = findViewById(R.id.n7);
+        final Button n8 = findViewById(R.id.n8);
+        final Button n9 = findViewById(R.id.n9);
+        final Button dot = findViewById(R.id.dot);
+        final Button equals = findViewById(R.id.equal);
+        final Button addition = findViewById(R.id.addition);
+        final Button substraction = findViewById(R.id.substraction);
+        final Button multi = findViewById(R.id.multiplication);
+        final Button div = findViewById(R.id.divison);
+
+
+        final View.OnClickListener calcListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int id = v.getId();
+                switch (id) {
+                    case R.id.n0:
+                        calcScreen.append("0");
+                        break;
+                    case R.id.n1:
+                        calcScreen.append("1");
+                        break;
+                    case R.id.n2:
+                        calcScreen.append("2");
+                        break;
+                    case R.id.n3:
+                        calcScreen.append("3");
+                        break;
+                    case R.id.n4:
+                        calcScreen.append("4");
+                        break;
+                    case R.id.n5:
+                        calcScreen.append("5");
+                        break;
+                    case R.id.n6:
+                        calcScreen.append("6");
+                        break;
+                    case R.id.n7:
+                        calcScreen.append("7");
+                        break;
+                    case R.id.n8:
+                        calcScreen.append("8");
+                        break;
+                    case R.id.n9:
+                        calcScreen.append("9");
+                        break;
+                    case R.id.dot:
+
+                        break;
+                    case R.id.divison:
+
+                        break;
+                    case R.id.equal:
+                        if(isOpPressed) {
+                            if(currentOp=='+'){
+                                String screenContent = calcScreen.getText().toString();
+                                String secondNumberString = screenContent.
+                                        substring(secondNumberIndex,screenContent.length());
+                                double secondNumber = Double.parseDouble(secondNumberString);
+                                secondNumber+=firstNum;
+                                calcScreen.setText(String.valueOf(secondNumber));
+                            }
+                        }
+                        break;
+                    case R.id.multiplication:
+
+                        break;
+                    case R.id.substraction:
+
+                        break;
+                    case R.id.addition:
+                        String screenContent = calcScreen.getText().toString();
+                        secondNumberIndex = screenContent.length()+1;
+                        firstNum=Double.parseDouble(screenContent);
+                        calcScreen.append("+");
+                        isOpPressed=true;
+                        currentOp = '+';
+                        break;
+                }
+            }
+        };
+        n0.setOnClickListener(calcListener);
+        n1.setOnClickListener(calcListener);
+        n2.setOnClickListener(calcListener);
+        n3.setOnClickListener(calcListener);
+        n4.setOnClickListener(calcListener);
+        n5.setOnClickListener(calcListener);
+        n6.setOnClickListener(calcListener);
+        n7.setOnClickListener(calcListener);
+        n8.setOnClickListener(calcListener);
+        n9.setOnClickListener(calcListener);
+        dot.setOnClickListener(calcListener);
+        equals.setOnClickListener(calcListener);
+        addition.setOnClickListener(calcListener);
+        substraction.setOnClickListener(calcListener);
+        multi.setOnClickListener(calcListener);
+        div.setOnClickListener(calcListener);
+
+        final Button delete = findViewById(R.id.del);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String displayNums = calcScreen.getText().toString();
+                int length = displayNums.length();
+                if (length>0) {
+                    displayNums = displayNums.substring(0,length-1);
+                    calcScreen.setText(displayNums);
+                }
+            }
+        });
+        final Button CE = findViewById(R.id.cleareverything);
+        CE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcScreen.setText("");
+            }
+        });
+    }
+}

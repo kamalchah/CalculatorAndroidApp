aafd99e771f0180812d4e6a38a55bd64f89441a7
Kamal <kamal200107@hotmail.com>
2020-09-01 20:41:54 -0400

    app now works

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,150 +1,141 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
-    private boolean isOpPressed = false;
-
-    private double firstNum = 0;
-
-    private int secondNumberIndex = 0;
-
-    private char currentOp;
+    private int[] numericButtons = {R.id.n0, R.id.n1, R.id.n2, R.id.n3, R.id.n4, R.id.n5, R.id.n6, R.id.n7, R.id.n8, R.id.n9};
+    // IDs of all the operator buttons
+    private int[] operatorButtons = {R.id.addition, R.id.substraction, R.id.multiplication, R.id.divison};
+    // TextView used to display the output
+    private TextView txtScreen;
+    // Represent whether the lastly pressed key is numeric or not
+    private boolean lastNumeric;
+    // Represent that current state is in error or not
+    private boolean stateError;
+    // If true, do not allow to add another DOT
+    private boolean lastDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView calcScreen = findViewById(R.id.calculatorScreen);
-        final Button n0 = findViewById(R.id.n0);
-        final Button n1 = findViewById(R.id.n1);
-        final Button n2 = findViewById(R.id.n2);
-        final Button n3 = findViewById(R.id.n3);
-        final Button n4 = findViewById(R.id.n4);
-        final Button n5 = findViewById(R.id.n5);
-        final Button n6 = findViewById(R.id.n6);
-        final Button n7 = findViewById(R.id.n7);
-        final Button n8 = findViewById(R.id.n8);
-        final Button n9 = findViewById(R.id.n9);
-        final Button dot = findViewById(R.id.dot);
-        final Button equals = findViewById(R.id.equal);
-        final Button addition = findViewById(R.id.addition);
-        final Button substraction = findViewById(R.id.substraction);
-        final Button multi = findViewById(R.id.multiplication);
-        final Button div = findViewById(R.id.divison);
 
+        //create Textview for display
+        this.txtScreen = (TextView) findViewById(R.id.calculatorScreen);
+//        final TextView calcScreen = findViewById(R.id.calculatorScreen);
+        setNumericOnClickListener();
 
-        final View.OnClickListener calcListener = new View.OnClickListener() {
+        setOperatorOnClickListener();
+    }
+    private void setNumericOnClickListener() {
+        // Create a common OnClickListener
+        View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final int id = v.getId();
-                switch (id) {
-                    case R.id.n0:
-                        calcScreen.append("0");
-                        break;
-                    case R.id.n1:
-                        calcScreen.append("1");
-                        break;
-                    case R.id.n2:
-                        calcScreen.append("2");
-                        break;
-                    case R.id.n3:
-                        calcScreen.append("3");
-                        break;
-                    case R.id.n4:
-                        calcScreen.append("4");
-                        break;
-                    case R.id.n5:
-                        calcScreen.append("5");
-                        break;
-                    case R.id.n6:
-                        calcScreen.append("6");
-                        break;
-                    case R.id.n7:
-                        calcScreen.append("7");
-                        break;
-                    case R.id.n8:
-                        calcScreen.append("8");
-                        break;
-                    case R.id.n9:
-                        calcScreen.append("9");
-                        break;
-                    case R.id.dot:
+                // Just append/set the text of clicked button
+                Button button = (Button) v;
+                if (stateError) {
+                    // If current state is Error, replace the error message
+                    txtScreen.setText(button.getText());
+                    stateError = false;
+                } else {
+                    // If not, already there is a valid expression so append to it
+                    txtScreen.append(button.getText());
+                }
+                // Set the flag
+                lastNumeric = true;
+            }
+        };
+        // Assign the listener to all the numeric buttons
+        for (int id : numericButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
 
-                        break;
-                    case R.id.divison:
-
-                        break;
-                    case R.id.equal:
-                        if(isOpPressed) {
-                            if(currentOp=='+'){
-                                String screenContent = calcScreen.getText().toString();
-                                String secondNumberString = screenContent.
-                                        substring(secondNumberIndex,screenContent.length());
-                                double secondNumber = Double.parseDouble(secondNumberString);
-                                secondNumber+=firstNum;
-                                calcScreen.setText(String.valueOf(secondNumber));
-                            }
-                        }
-                        break;
-                    case R.id.multiplication:
-
-                        break;
-                    case R.id.substraction:
-
-                        break;
-                    case R.id.addition:
-                        String screenContent = calcScreen.getText().toString();
-                        secondNumberIndex = screenContent.length()+1;
-                        firstNum=Double.parseDouble(screenContent);
-                        calcScreen.append("+");
-                        isOpPressed=true;
-                        currentOp = '+';
-                        break;
+    /**
+     * Find and set OnClickListener to operator buttons, equal button and decimal point button.
+     */
+    private void setOperatorOnClickListener() {
+        // Create a common OnClickListener for operators
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If the current state is Error do not append the operator
+                // If the last input is number only, append the operator
+                if (lastNumeric && !stateError) {
+                    Button button = (Button) v;
+                    txtScreen.append(button.getText());
+                    lastNumeric = false;
+                    lastDot = false;    // Reset the DOT flag
                 }
             }
         };
-        n0.setOnClickListener(calcListener);
-        n1.setOnClickListener(calcListener);
-        n2.setOnClickListener(calcListener);
-        n3.setOnClickListener(calcListener);
-        n4.setOnClickListener(calcListener);
-        n5.setOnClickListener(calcListener);
-        n6.setOnClickListener(calcListener);
-        n7.setOnClickListener(calcListener);
-        n8.setOnClickListener(calcListener);
-        n9.setOnClickListener(calcListener);
-        dot.setOnClickListener(calcListener);
-        equals.setOnClickListener(calcListener);
-        addition.setOnClickListener(calcListener);
-        substraction.setOnClickListener(calcListener);
-        multi.setOnClickListener(calcListener);
-        div.setOnClickListener(calcListener);
-
-        final Button delete = findViewById(R.id.del);
-        delete.setOnClickListener(new View.OnClickListener() {
+        // Assign the listener to all the operator buttons
+        for (int id : operatorButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+        // Decimal point
+        findViewById(R.id.dot).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String displayNums = calcScreen.getText().toString();
-                int length = displayNums.length();
-                if (length>0) {
-                    displayNums = displayNums.substring(0,length-1);
-                    calcScreen.setText(displayNums);
+                if (lastNumeric && !stateError && !lastDot) {
+                    txtScreen.append(".");
+                    lastNumeric = false;
+                    lastDot = true;
                 }
             }
         });
-        final Button CE = findViewById(R.id.cleareverything);
-        CE.setOnClickListener(new View.OnClickListener() {
+        // Clear button
+        findViewById(R.id.cleareverything).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calcScreen.setText("");
+                txtScreen.setText("");  // Clear the screen
+                // Reset all the states and flags
+                lastNumeric = false;
+                stateError = false;
+                lastDot = false;
             }
         });
+        // Equal button
+        findViewById(R.id.equal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEqual();
+            }
+        });
+    }
+
+    /**
+     * Logic to calculate the solution.
+     */
+    private void onEqual() {
+        // If the current state is error, nothing to do.
+        // If the last input is a number only, solution can be found.
+        if (lastNumeric && !stateError) {
+            // Read the expression
+            String txt = txtScreen.getText().toString();
+            // Create an Expression (A class from exp4j library)
+            Expression expression = new ExpressionBuilder(txt).build();
+            try {
+                // Calculate the result and display
+                double result = expression.evaluate();
+                txtScreen.setText(Double.toString(result));
+                lastDot = true; // Result contains a dot
+            } catch (ArithmeticException ex) {
+                // Display an error message
+                txtScreen.setText("Error");
+                stateError = true;
+                lastNumeric = false;
+            }
+        }
     }
 }
